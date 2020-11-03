04d0d539d29da2786696570021e08cd7fcf99213
Aleksandr Kurylev <adkurylev@gmail.com>
2020-11-03 23:35:51 +0300

    Initial commit

---
+++ Task_01/Program.cs
@@ -0,0 +1,133 @@
+﻿/* В библиотеке классов Cinderella определите абстрактный класс Something.  
+ * От класса Something  унаследуйте два класса: Lentil, Ashes. В Классе Lentil 
+ * определите поле Weight, принимающее для каждого нового экземпляра класса 
+ * случайное вещественное значение в интервале [0, 2]. В классе Ashes определите 
+ * поле Volume, принимающее для каждого нового экземпляра класса случайное 
+ * вещественное значение в интервале [0, 1]​.
+ * 
+ * В основной программе создайте массив N (N ввести с клавиатуры) экземпляров 
+ * классов Lentil и Ashes (принадлежность очередного элемента массива к первому 
+ * или второму классу определите при помощи датчика случайных чисел). 
+ * Выведите массив на экран. Из элементов массива сформируйте и выведите на экран 
+ * два списка экземпляров Lentil  и Ashes. ​
+ * 
+ * Double выводить с точностьюдо двух знаков после запятой!
+ * 
+ * 
+ * Формат входных данных : 
+ * -------test_1-------
+ * -1
+ * -------test_2-------
+ * 4
+ * 0,57 
+ * 1,42 
+ * 0,91 
+ * 1,12
+ * --------------------
+ * 
+ * Формат выходных данных : 
+ * -------test_1-------
+ * Incorrect input!
+ * -------test_2-------
+ * 0,57 1,42 0,91 1,12
+ * 0,57 0,91
+ * 1,42 1,12
+ * --------------------
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_01
+{
+    class Program
+    {
+        private static readonly Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int n;
+            //добавьте проверку на некорректную длинну массива
+            if (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Incorrect input!");
+            }
+    
+            Something[] array = CreateArray(n);
+            PrintArray(array);
+            PrintSeparately(array);
+        }
+
+        static Something[] CreateArray(int n)
+        {
+            Something[] array = new Something[n];
+
+            /*Заполните массив array n элементами (Lentil/Ashes) 
+             *вводом чисел с клавиатуры (array[i] = new Lentil(1.06);)
+             *В случае некорретного значения вывести "Incorrect input!" и продолжить ввод,
+             * пропустив этот элемент.
+             * ...*/
+
+            return array;
+        }
+
+        static void PrintArray(Something[] array)
+        {
+            /*Выведите массив на экран в одну строку, 
+             * разделяя элементы пробелами. Не забудьте переопредлить ToString().
+             * ...*/
+        }
+
+        static void PrintSeparately(Something[] array)
+        {
+            /*выведите в одну строку элементы типа Lentil, 
+             * затем с новой строки элементы типа Ashes.
+             * Также через пробел!*/
+            string lentils = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] is Lentil)
+                {
+                    lentils += $"{array[i]} ";
+                }
+            }
+            Console.WriteLine(lentils);
+            //вывод элементов Ashes реализуйте самостоятельно
+            //...
+
+            /*Реализация для примера, которая забанена в рамках данного дз.
+             *var lentilsCollection = array.Where(x => x is Lentil);
+             *Console.WriteLine(String.Join(" ", lentilsCollection));*/
+        }
+    }
+
+    abstract class Something
+    {
+    }
+
+    class Lentil : Something
+    {
+        private double weight;
+        public Lentil(double weight)
+        {
+            Weight = weight;
+        }
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+    }
+
+    //реализуйте класс Ashes по аналогии с Lentil, используя auto-свойства!
+    class Ashes : Something
+    {
+        
+    }
+}
---
+++ Task_02/Program.cs
@@ -0,0 +1,223 @@
+﻿/*
+ * Oпределить абстрактный класс Animal. 
+ * В классе описать поле «кличка животного», «возраст животного», методы 
+ * доступа к полям. Снабдить класс Animal прототипами методов AnimalSound() - 
+ * «звук животного» и AnimalInfo() – информация о животном 
+ * (значения всех его полей). Описать два класса наследника 
+ * от Animal. Класс Dog, описывающий собаку и класс Cow, описывающий корову. 
+ * Класс Dog инкапсулирует поля «порода собаки» и логическое поле isTrained 
+ * (true, если собака дрессирована), добавить методы доступа к полям. 
+ * Класс Cow инкапсулирует поле «количество молока в день», добавить метод доступа 
+ * к полям. В случае некорректных данных методы и конструкторы должны порождать 
+ * исключения. В каждом классе переопределить методы AnimalSound() и AnimalInfo() 
+ * в соответствии с типом животного. В консольном приложениии на основе данных, 
+ * полученных от пользователя породить объект класса Dog и Cow, вывести информацию 
+ * и звуки животных. В программе предусмотреть защиту от исключений. ​ 
+ * 
+ * Формат входных данных : 
+ * -------test_1-------
+ * Rex
+ * 3
+ * hunter
+ * true
+ * Murka
+ * 8
+ * 10
+ * -------test_2-------
+ * Rex
+ * -3
+ * hunter
+ * true
+ * Murka
+ * 8
+ * 10
+ * --------------------
+ * 
+ * Формат выходных данных : 
+ * -------test_1-------
+ * Woof-woof!
+ * Name: Rex
+ * Age: 3
+ * Breed: hunter
+ * Trained: True
+ * Moo-moo!
+ * Name: Murka
+ * Age: 8
+ * Milk per day: 10
+ * -------test_2-------
+ * Incorrect input!
+ * --------------------
+ */
+
+using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //самостоятельно оберните нужный блок(и) в try-catch
+            //в catch выводите на экран "Incorrect input!"
+            Dog dog;
+            Cow cow;
+
+            string dogName = Console.ReadLine();
+            int dogAge = IntInput();
+            string breed = Console.ReadLine();
+            bool isTrained = BoolInput();
+
+            string cowName = Console.ReadLine();
+            int cowAge = IntInput();
+            int milkQuantity = IntInput();
+
+            dog = new Dog(dogName, dogAge, breed, isTrained);
+            dog.AnimalSound();
+            Console.WriteLine(dog.AnimalInfo());
+
+            cow = new Cow(cowName, cowAge, milkQuantity);
+            cow.AnimalSound();
+            Console.WriteLine(cow.AnimalInfo());
+        }
+
+        static bool BoolInput()
+        {
+            //обработать строковый ввод пользователя ("true"/"false")
+        }
+
+        static int IntInput()
+        {
+            //обработать ввод integer, в случае, если пользователь ввел не 
+            //int выбросить Exception (обрабатывать не нужно).
+        }
+    }
+
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                //выбросьте исключение, если введена пустая строка
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                //выбросьте исключение, если введена пустая строка
+                age = value;
+            }
+        }
+
+        public abstract void AnimalSound();
+
+        public abstract string AnimalInfo();
+    }
+
+    public class Dog : Animal
+    {
+        private string breed;
+        private bool isTrained;
+
+        public Dog(string name, int age, string breed, bool isTrained)
+        {
+            //реализуйте конструктор
+        }
+
+        public string Breed
+        {
+            get
+            {
+                return breed;
+            }
+            private set
+            {
+                //выбросьте исключение, если введена пустая строка
+                breed = value;
+            }
+        }
+
+        public bool IsTrained
+        {
+            get
+            {
+                return isTrained;
+            }
+            private set
+            {
+                isTrained = value;
+            }
+        }
+        public override string AnimalInfo()
+        {
+            return ToString();
+        }
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Woof-woof!");
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}{Environment.NewLine}" +
+                   $"Age: {Age}{Environment.NewLine}" +
+                   $"Breed: {Breed}{Environment.NewLine}" +
+                   $"Trained: {IsTrained}{Environment.NewLine}";
+        }
+    }
+
+    public class Cow : Animal
+    {
+        private int milkQuantity;
+
+        public Cow(string name, int age, int milkQuantity)
+        {
+            //реализуйте конструктор
+        }
+
+        public int MilkQuantity 
+        {
+            get => milkQuantity; 
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception();
+                }
+                milkQuantity = value;
+            }
+        }
+
+        public override string AnimalInfo()
+        {
+            return ToString();
+        }
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Moo-moo!");
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}{Environment.NewLine}" +
+                   $"Age: {Age}{Environment.NewLine}" +
+                   $"Milk per day: {MilkQuantity}{Environment.NewLine}";
+        }
+    }
+}
